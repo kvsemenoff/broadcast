9ef172b2cd32b365eaaa2eed27e6f4e4423adfd4
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-04-19 01:20:30 +0600

    didn't end header-top

--- js/common.js
+++ js/common.js
@@ -114,34 +114,21 @@ $(document).ready(function(){
 		});
 
 
-		// ===========================================
-		// ============ АККОРДЕОН ДЛЯ МЕНЮ ===========
-		// ===========================================
-			$('.menu-up').slideUp(0);
-			$('.menu-button').click(function(){
-				if (!$(this).hasClass('active')){
-					$(this).addClass('active');
-					$('.menu-up').slideDown(400)
-				} else {
-					$(this).removeClass('active');
-					$('.menu-up').slideUp(400)
-				}
-			});
+
+	$('.menu-up').slideUp(0);
+	$('.menu-button').click(function(){
+		if (!$(this).hasClass('menu-up-active')){
+			$(this).addClass('menu-up-active');
+			$('.menu-up').slideDown(400)
+		} else {
+			$(this).removeClass('menu-up-active');
+			$('.menu-up').slideUp(400)
+		}
+	});
 
 
-			// ===========================================
-			// ============ АККОРДЕОН ДЛЯ ПОД-МЕНЮ =======
-			// ===========================================
-				$('.submenu').slideUp(0);
-				$('.submenu-button').click(function(){
-					if (!$(this).hasClass('active')){
-						$(this).addClass('active');
-						$('.submenu').slideDown(400)
-					} else {
-						$(this).removeClass('active');
-						$('.submenu').slideUp(400)
-					}
-				});
+
+	
 });
 
 

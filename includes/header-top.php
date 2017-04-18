44591d692c258cbe7240088c82b1fe4f9f3893c4
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-04-18 23:27:26 +0600

    some changes

---
+++ includes/header-top.php
@@ -0,0 +1,89 @@
+<div class="header">
+	<div class="container">
+		<div class="menu-g">
+			<nav class="menu">
+				<ul class="menu__list">
+					<li class="menu__li">
+						<a href="#">Главная</a>
+						<ul class="sub">
+							<li class="sub__li"><a href="#">Новости</a></li>
+							<li class="sub__li"><a href="#">О нашей ФИРМЕ</a></li>
+							<li class="sub__li"><a href="#">контакты</a></li>
+						</ul>
+					</li>
+					<li class="menu__li">
+						<a href="#">Купить POWERBOX</a>
+						<ul class="sub">
+							<li class="sub__li"><a href="#">Купить POWERBOX</a></li>
+							<li class="sub__li"><a href="#">Купить POWERBOX</a></li>
+							<li class="sub__li"><a href="#">Купить POWERBOX</a></li>
+						</ul>
+					</li>
+					<li class="menu__li">
+						<a href="#">Преимущества</a>
+						<ul class="sub">
+							<li class="sub__li"><a href="#">Преимущества</a></li>
+							<li class="sub__li"><a href="#">Преимущества</a></li>
+							<li class="sub__li"><a href="#">Преимущества</a></li>
+						</ul>
+					</li>
+					<li class="menu__li">
+						<a href="#">Jetbox</a>
+						<ul class="sub">
+							<li class="sub__li"><a href="#">Jetbox</a></li>
+							<li class="sub__li"><a href="#">Jetbox</a></li>
+							<li class="sub__li"><a href="#">Jetbox</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="menu-burger clearfix">
+			<h2>МЕНю бургер</h2>
+			<div class="menu-button">Меню
+				<i class="bg"></i>
+			</div>
+			<nav class="menu-up">
+				<ul class="menu__list">
+					<li class="menu__li">
+						<a href="#" class="submenu-button">Главная</a>
+						<ul class="submenu">
+							<li><a href="#">Контакты</a></li>
+							<li><a href="#">Контакты</a></li>
+							<li><a href="#">Контакты</a></li>
+							<li><a href="#">Контакты</a></li>
+							<li><a href="#">Контакты</a></li>
+						</ul>
+					</li>
+					<li class="menu__li">
+						<a href="#">Купить POWERBOX</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Преимущества</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Jetbox</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Установочные центры</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Вопросы и ответы</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Видео</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Онлайн трансляция</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Отзывы</a>
+					</li>
+					<li class="menu__li">
+						<a href="#">Разработка</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</div>

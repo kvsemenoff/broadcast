4983bc629babf173eb92bc245a84e6cb5852d194
kvsemenoff <kvsemenoff@mail.ru>
2017-04-19 18:46:51 +0400

    d

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Работа касс в автономной зоне: нюансы применения нового ФЗ-54</title>
+	<title>Главная</title>
 	
 	<link rel="shortcut icon" href="favicon.png" />
 

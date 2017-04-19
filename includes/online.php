ad39c5e80875ac8e7f7bbb9469b12bbe56ec1965
engine77 <zaur.1kager@gmail.com>
2017-04-19 11:46:39 +0400

    waht common

---
+++ includes/online.php
@@ -0,0 +1,20 @@
+<div class="online online_bg_img">
+	<div class="online__container">
+		<h2 class="h2_theme_rosa h2_pink online__topcaption">СТОИМОСТЬ ОНЛАЙН ТРАНСЛЯЦИИ НА ЭКРАН</h2>
+		<div class="online__camers">
+			<div class="online__cam_box">
+				<h4 class="online__boxcaption">1 КАМЕРА</h4>
+				<span class="online__boxsub">На сцене 1 спикер</span>
+				<p class="online__text">Персонал на видеоплощадке: <br> 1 видеооператор, 1 видеоинженер</p>
+				<span class="online__pricetext">от <em>15 000</em><i class="fa fa-rub" aria-hidden="true"></i></span>
+				<a href="#" class="button button_theme_rosa button_order">Заказать</a>
+			</div>
+		</div>
+		<div class="online__textbottom">
+			<h4 class="online__textbottom_caption">Указана минимальная комплектация.</h4>
+			<p class="online__textbottom_txt">Дополнительное ообрудование: операторские краны, тележки, lightbridge, экраны, освещение на площадке и прочее - обсуждается дополнительно.
+			Для индивидуального расчета мероприятия заполните форму и укажите все детали, пожелания и требуемые опции.</p>
+			<a href="#" class="button button_theme_rosa ">Получить КП и смету</a>
+		</div>
+	</div>
+</div>	

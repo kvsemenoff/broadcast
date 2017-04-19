d4aca91f42aee7737e7fb24dca08c3b2882a421a
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-04-20 01:27:12 +0600

    fix bugs on popup.php

--- popup.php
+++ popup.php
@@ -1,15 +1,12 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
-<!-- Large modal -->
-<button class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Большая модаль</button>
-
-<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade bs-example-modal-lg">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
 			<h2 class="h2_theme_rosa h2_pink whattodo__topcaption">Ваше сообщение отправлено!</h2>
       <div class="modal__descr-block">
       	<span class="modal__descr">Спасибо за вашу заявку.</span>
-				<span class="modal__descr">В скором времени мы напишем вам на почту.</span>
+				<span class="modal__descr"><i>В скором времени </i><i>мы напишем вам на почту.</i></span>
 			</div>
     </div>
   </div>

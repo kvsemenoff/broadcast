704a7d30eb8d3827acfeb32df8f080cb64cf78e6
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-04-19 14:45:28 +0600

    begin contact.php

--- contacts.php
+++ contacts.php
@@ -1,4 +1,29 @@
 <?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header-top.php'); ?>
+
+<div class="contact">
+	<div class="container">
+		<h2 class="h2_theme_rosa h2_pink whattodo__topcaption">Наши контакты</h2>
+		<div class="contact__inner">
+			<div class="contact__mape">
+				
+			</div>
+			<div class="contact__info info">
+				<span class="info__title">Наш офис</span>
+				<span class="info__address">Каменоостровский пр., д. 26-28,</span>
+				<span class="info__time">с 11:00 до 19:00</span>
+				<span class="info__number">+7 (921) 000-12-05</span>
+				<span class="info__mail">info@droadcast-team.ru</span>
+				<div class="socials">
+			    <a href="#" class="socials-link socials-vk"></a>
+			    <a href="#" class="socials-link socials-youtube"></a>
+			    <a href="#" class="socials-link socials-insta"></a>
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="n-wrapper">
 	<div class="n-header">
 	</div>
